3b5d63734800bae4cf73fde992674ee96780f235
hninmabalo <hninmabalo@gmail.com>
2022-09-10 15:08:14 -0700

    Create env variable and add session and flash middleware

--- models/user.js
+++ models/user.js
@@ -1,4 +1,6 @@
 'use strict';
+const bcrypt = require('bcryptjs');
+
 const {
   Model
 } = require('sequelize');
@@ -14,12 +16,54 @@ module.exports = (sequelize, DataTypes) => {
     }
   }
   user.init({
-    name: DataTypes.STRING,
-    email: DataTypes.STRING,
-    password: DataTypes.STRING
+    name: {
+      type: DataTypes.STRING,
+      validate: {
+       len: {
+        args: [1,99],
+        msg: 'Name must be between 1 and 99 characters'
+       }
+      }
+    },
+    email: {
+      type: DataTypes.STRING,
+      validate: {
+        isEmail: {
+          msg: 'Invalid email'
+        }
+      }
+    },
+    password: {
+      type: DataTypes.STRING,
+      validate: {
+        len: {
+          args: [8,99],
+          msg: 'Password must be between 8 and 99 characters'
+        }
+      }
+    }
   }, {
     sequelize,
     modelName: 'user',
   });
-  return user;
+
+user.addHook('beforeCreate', (pendingUser) => {
+  let hash = bcrypt.hashSync(pendingUser.password, 12);
+  pendingUser.password = hash;
+});
+
+user.prototype.validatePassword = function(typedPassword) {
+  let isCorrectPassword = bcrypt.compare(typedPassword, this.password);
+
+  return isCorrectPassword;
+}
+
+user.prototype.toJSON = function() {
+  let userData = this.get(); 
+  delete userData.password; // it doesn't delete password from database, only removes it. 
+  
+  return userData;
+}
+
+  return user; // add functions above 
 };
